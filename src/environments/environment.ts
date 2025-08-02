c327d0a873a1cc7b5ba3c437433436192fbd2176
Agustina Rojas <agusrojasmc@gmail.com>
2025-08-02 15:59:01 -0300

    subo environments para deploy

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://geographia-api-production.up.railway.app/api',
+    mapbox: {
+        accessToken: '',
+        baseUrl: 'https://api.mapbox.com/search/geocode/v6',
+    },
+};
