956ed1cd695c0778cec9d37370834db5ffffe8a5
Agustina Rojas <agusrojasmc@gmail.com>
2025-08-02 16:47:40 -0300

    arreglo el get

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -231,12 +231,16 @@ export class MapComponent {
             }
         });
 
-        this.http.get('argentina-mask.geojson').subscribe((argentina: any) => {
-            const world = bboxPolygon([-180, -90, 180, 90]);
-            this.maskGeoJSON = difference(
-                featureCollection([world, ...argentina.features])
-            );
-        });
+        if (this.isBrowser) {
+            this.http
+                .get('argentina-mask.geojson')
+                .subscribe((argentina: any) => {
+                    const world = bboxPolygon([-180, -90, 180, 90]);
+                    this.maskGeoJSON = difference(
+                        featureCollection([world, ...argentina.features])
+                    );
+                });
+        }
 
         this.routerSub = this.router.events
             .pipe(filter((event) => event instanceof NavigationEnd))
@@ -266,25 +270,33 @@ export class MapComponent {
     }
 
     onMapClick(event: mapboxgl.MapMouseEvent) {
-        this.http.get('argentina-mask.geojson').subscribe((argentina: any) => {
-            const clickedPoint = point([event.lngLat.lng, event.lngLat.lat]);
+        if (this.isBrowser) {
+            this.http
+                .get('argentina-mask.geojson')
+                .subscribe((argentina: any) => {
+                    const clickedPoint = point([
+                        event.lngLat.lng,
+                        event.lngLat.lat,
+                    ]);
 
-            const withinArgentina = argentina.features.some((feature: any) =>
-                booleanPointInPolygon(clickedPoint, feature)
-            );
+                    const withinArgentina = argentina.features.some(
+                        (feature: any) =>
+                            booleanPointInPolygon(clickedPoint, feature)
+                    );
 
-            if (!withinArgentina) {
-                return;
-            }
+                    if (!withinArgentina) {
+                        return;
+                    }
 
-            if (this.popup) {
-                this.popup = null;
-            } else {
-                this.popup = {
-                    coordinates: [event.lngLat.lng, event.lngLat.lat],
-                };
-            }
-        });
+                    if (this.popup) {
+                        this.popup = null;
+                    } else {
+                        this.popup = {
+                            coordinates: [event.lngLat.lng, event.lngLat.lat],
+                        };
+                    }
+                });
+        }
     }
 
     ngAfterViewInit() {
